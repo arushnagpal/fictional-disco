fe687f1b2fdcc2a9b2db07181ea0d2ab801a2628
Arush Nagpal <Arush.Nagpal@walmart.com>
2019-05-14 00:57:06 +0530

    added problems

---
+++ leetcode/53.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+       int max_so_far = a[0]; 
+       int curr_max = a[0]; 
+
+       for (int i = 1; i < a.size(); i++) 
+       { 
+            curr_max = max(a[i], curr_max+a[i]); 
+            max_so_far = max(max_so_far, curr_max); 
+       } 
+       return max_so_far; 
+    }
+};
